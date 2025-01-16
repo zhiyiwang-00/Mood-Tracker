34ade200643aab8377c20a316b36e6ffc9119ba2
Zhiyi Wang <zhiyi.wang@loopacademy.no>
2025-01-16 13:51:09 +0100

    FEAT: complete styling

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -22,9 +22,8 @@ export function Navbar() {
           <div className="navigation">
             {/* TODO: set startupPage as default page*/}
             <a href="/moodSelector" className="nav_table">Mood Selector</a>
-            <a href="/profilePage" className="nav_table">Dean <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
-  <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
-</svg>
+            <a href="/profilePage" className="nav_table">Dean <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-fill" viewBox="0 0 16 16">
+            <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/></svg>
             </a> 
             <button id="logout_button">Logout</button> {/*Go back to startup page when click on*/}
 
