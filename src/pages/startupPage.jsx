578d14db0fcbf36b8669d07583915ca6938dce3e
Zhiyi Wang <zhiyi.wang@loopacademy.no>
2025-01-16 15:48:47 +0100

    Nothing special

--- src/pages/startupPage.jsx
+++ src/pages/startupPage.jsx
@@ -1,8 +1,21 @@
 export function Startup(){
+//   const inputName = document.getElementById('user_name');
+//   inputName.addEventListener('submit', async function (e) {
+//     // e.preventDefault();
+//     // const formData = new FormData(inputName).entries()
+//     // const response = await fetch('https://reqres.in/api/users', {
+//     //     method: 'POST',
+//     //     headers: { 'Content-Type': 'application/json' },
+//     //     body: JSON.stringify(Object.fromEntries(formData))
+//     });
+
+//     // const result = await response.json();
+//     // console.log(result)
+// });
     return (
         <>
         <div className="login-window">
-          <input type="text" placeholder="Enter your name" />
+          <input type="text" placeholder="Enter your name" id="user_name" />
           <br /><br />
           <button>Start</button>
         </div>
