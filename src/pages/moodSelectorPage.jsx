42dd9c537b680642dba6ff6ca7dfd43878a79262
Teresa Pham <teresapham295@gmail.com>
2025-01-17 10:22:34 +0100

    FEAT: make mood buttons into a separate component

--- src/pages/moodSelectorPage.jsx
+++ src/pages/moodSelectorPage.jsx
@@ -1,42 +1,46 @@
-import { useQuery } from "react-query";
-
-const fetchData = async () => {
-  const response = await fetch("https://troubled-fuchsia-crocodile.glitch.me/moods"); // Adjust the URL if needed
-  if (!response.ok) {
-    throw new Error("Network response was not ok");
-  }
-  const data = await response.json();
-  return data;
-};
+import { MoodButtons } from "../components/moodButtons";
+import { useState } from "react";
 
 export function MoodSelector() {
-  const { data, isLoading, error } = useQuery("moods", fetchData); // 'moods' is the unique key for this query
+  const [selectedMoods, setSelectedMoods] = useState([]);
+  const [submittedMoods, setSubmittedMoods] = useState([]);
+  const [resetToggle, setResetToggle] = useState(false);
 
-  if (isLoading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
+  const handleMoodSelection = (moods) => {
+    setSelectedMoods(moods); 
+  };
+
+  const submit = () => {
+    setSubmittedMoods(selectedMoods); 
+    setSelectedMoods([]);
+    setResetToggle((prev) => !prev); 
+  };
 
   return (
-    <>
-      <div id="mainContainer">
-        <div
-          id="container"
-          className="d-flex flex-column justify-content-around"
-        >
-          <div className="d-flex flex-row justify-content-center flex-wrap">
-            {data.map((mood) => (
-              <button key={mood.id} className="btn btn-light rounded-pill moodBtn">{mood.name} {mood.emoji}</button>
-            ))}
-          </div>
-          <div>
-            <p className="fs-4">
-              Last mood entry: <span>😀😀😀</span>
-            </p>
-          </div>
-          <div className="d-flex flex-row justify-content-end">
-            <button className="btn btn-dark rounded">Submit</button>
-          </div>
+    <div id="mainContainer">
+      <div id="container" className="d-flex flex-column justify-content-around">
+        <MoodButtons
+          onMoodSelectionChange={handleMoodSelection}
+          resetToggle={resetToggle} 
+        />
+        <div>
+          <p className="fs-4">
+            Last mood entry: <span>{selectedMoods.map((mood) => mood.emoji).join(" ") || "None"}</span>
+          </p>
+          <p id="moodEntry" className="fs-4">
+            Mood entry: <span>{submittedMoods.map((mood) => mood.emoji).join(" ")}</span>
+          </p>
+        </div>
+        <div className="d-flex flex-row justify-content-end">
+          <button
+            className="btn btn-dark rounded"
+            onClick={submit}
+            disabled={selectedMoods.length === 0} 
+          >
+            Submit
+          </button>
         </div>
       </div>
-    </>
+    </div>
   );
 }
