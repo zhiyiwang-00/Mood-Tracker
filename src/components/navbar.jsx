0796b4ba42d3d2e2efa8a7c3c89257632a22c4f1
Teresa Pham <teresapham295@gmail.com>
2025-01-17 12:54:07 +0100

    FEAT: hide bnav buttons when not logged in

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,28 +1,41 @@
 export function Navbar() {
-    // const [count, setCount] = useState(0)
 
-    function logoutUser(){
-      localStorage.removeItem("user");
-      window.location.href = "/";
-    }
-    
-    let loggedInUser = JSON.parse(localStorage.getItem("user"));
-
-    return (
-      <>
-      <div className="nav-bar">
-          <p className="title">Welcome to Mood Tracker 😁</p>
-          <div className="navigation">
-            {/* TODO: set startupPage as default page*/}
-            <a href="/moodSelector" className="nav_table">Mood Selector</a>
-            <a href="/profilePage" className="nav_table"> {loggedInUser ? loggedInUser.username : ""} <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-fill" viewBox="0 0 16 16">
-            <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/></svg>
-            </a> 
-            <button id="logout_button" onClick={logoutUser}>Logout</button> {/*Go back to startup page when click on*/}
-
-      </div> 
-      </div>
-      </> 
-    )
+  function logoutUser() {
+    localStorage.removeItem("user");
+    window.location.href = "/";
   }
-  
+
+  let loggedInUser = JSON.parse(localStorage.getItem("user"));
+
+  return (
+    <>
+      <div className="nav-bar">
+        <p className="title">Welcome to Mood Tracker 😁</p>
+        {loggedInUser && (
+            <div className="navigation">
+              {/* TODO: set startupPage as default page*/}
+              <a href="/moodSelector" className="nav_table">
+                Mood Selector
+              </a>
+              <a href="/profilePage" className="nav_table">
+                {loggedInUser ? loggedInUser.username : ""}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-person-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
+                </svg>
+              </a>
+              <button id="logout_button" onClick={logoutUser}>
+                Logout
+              </button>
+            </div>
+          )}
+      </div>
+    </>
+  );
+}
