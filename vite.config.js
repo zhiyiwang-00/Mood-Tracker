546386ee98815f79b8330b4972abb51bf3be5d58
Zhiyi Wang <zhiyi.wang00@gmail.com>
2025-01-21 08:18:38 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://mood-tracker-onvfyebrs-zhiyiwang-00s-projects.vercel.app/"
 })
