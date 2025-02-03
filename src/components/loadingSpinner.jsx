55357d22bb184ab595a06eee14cf2d80e0eb1ff4
Jobb <jobb@MacBook-Pro.local>
2025-02-03 11:25:23 +0100

    FEAT: add spinner component

---
+++ src/components/loadingSpinner.jsx
@@ -0,0 +1,9 @@
+export function LoadingSpinner() {
+    return (
+        <div className="d-flex justify-content-center">
+            <div className="spinner-border text-secondary" role="status">
+                <span className="sr-only"></span>
+            </div>
+        </div>
+    )
+}
